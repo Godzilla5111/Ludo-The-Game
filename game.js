442b3de566f40a89447d1ffb49a006bb77c9f29b
Anurag Singh <62075225+Godzilla5111@users.noreply.github.com>
2020-06-08 13:24:05 +0530

    Update game over conditions

--- game.js
+++ game.js
@@ -693,6 +693,9 @@ function diceRollA() {
     } else {
       alert("PlayerB won the game! Refresh the page to play again!");
       $('h1').html(playerB + " won the game!🥇");
+      $('.turnA').html('');
+      $('.turnB').html('');
+      $('.roll').html("Game<br> Over!");
       var won = new Audio('applause.mp3');
       won.play();
     }
@@ -929,6 +932,9 @@ function diceRollB() {
     } else {
       alert("PlayerA won the game! Refresh the page to play again!");
       $('h1').html("🥇" + playerA + " won the game!");
+      $('.turnA').html('');
+      $('.turnB').html('');
+      $('.roll').html("Game<br> Over!");
       var won = new Audio('applause.mp3');
       won.play();
     }
